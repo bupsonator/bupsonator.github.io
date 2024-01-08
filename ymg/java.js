0faaa2a2409d64bdc6c928300d1fab561a63a105
highstake17 <147749937+highstake17@users.noreply.github.com>
2024-01-08 13:05:26 -0700

    papas

--- ymg/java.js
+++ ymg/java.js
@@ -1,2 +1,30 @@
-var x = document.getElementById("frm").src;
-document.getElementById("fs").href = x;
+// initialize some vars i need
+var gridChildren = document.getElementById("asdf").children;
+const grid = document.getElementById('asdf');
+
+// log the grid children to the console
+console.log("grid children " + gridChildren);
+
+// initialize the index counter
+var index = 0;
+
+// constantly scan for button clicks, then run newSrc() 
+for (let i = 0, len = grid.children.length; i < len; i++)
+{
+    grid.children[i].onclick = function()
+    {
+    index = i;
+    console.log("index of clicked: " + index);
+    newSrc();
+    }
+}
+
+// assign the button's value to the frm's src, then clear the menu, and display the window
+function newSrc() {
+    index = gridChildren[index];
+    var newSrc = index.value;
+    document.getElementById("frm").src = newSrc;
+    document.getElementById("fs").href = newSrc;
+    document.getElementById("window").style.display="block";
+    document.getElementById("menu").style.display="none";
+}
