ec630feba69f1f527416ebdc9111d6fa3cff6221
highstake17 <147749937+highstake17@users.noreply.github.com>
2024-01-11 09:11:28 -0700

    new interaction update

--- interactions/java.js
+++ interactions/java.js
@@ -47,9 +47,31 @@ function third() {
     {
         document.getElementById("highlight").innerHTML = "whoops, try again. highlight this line of text and click the button."
     }
-    else if (selection == "highlight this text, then click" || selection == "whoops, try again. highlight this line of text and click the button.")
+    else if (selection == "highlight this text, then click 'submit'" || selection == "whoops, try again. highlight this line of text and click the button." || selection == "try to be more precise, cuz my scripting is bad.")
     {
         document.getElementById("third").style.display="none";
         document.getElementById("fourth").style.display="block";
     }
+    else 
+    {
+        document.getElementById("highlight").innerHTML = "try to be more precise, cuz my scripting is bad."
+    }
+}
+
+function fourth() {
+    let selection = window.getSelection();
+    if (selection == "")
+    {
+        document.getElementById("quiz").innerHTML = "make sure to highlight an answer.";
+    }
+    else if (selection == "b) wrong")
+    {
+        document.getElementById("fourth").style.display="none";
+        document.getElementById("fifth").style.display="block";
+    }
+    else
+    {
+        document.getElementById("quiz").innerHTML = "make sure to be precise in your highlighting pleeeease";
+    }
+    
 }
