69fb7c4f8502e220d223c5972fea363253448a99
highstake17 <147749937+highstake17@users.noreply.github.com>
2024-01-16 17:02:18 -0700

    ANGER!!! PISSED!!!!! WOOOOOOO

---
+++ anger/java.js
@@ -0,0 +1,8 @@
+// JavaScript code for functionality (zzzcode.ai helped with this)
+const form = document.querySelector('form');
+const searchBox = document.querySelector('#box');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const searchTerm = searchBox.value;
+  alert(`YEAH ANGER!!!!!!!!!!! BURN IT!!! ARSON!!!!!`);
+});
