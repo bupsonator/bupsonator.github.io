70cc80d7f9a031d42b9edb19abad080535e1544f
highstake17 <147749937+highstake17@users.noreply.github.com>
2024-01-19 11:55:47 -0700

    new javerscrpit

--- javerscrips/java.js
+++ javerscrips/java.js
@@ -1,3 +1,7 @@
+// ------------------------------------------------------------------
+// |               THE IMPLEMENTATION JAVA BEGINs HERE              |
+// ------------------------------------------------------------------
+
 // terminal var for ease of use for me
 var terminal = document.getElementById("terminal");
 // clear the terminal in the site
@@ -27,7 +31,19 @@ function input(question)
     }
     return intput;
 }
-
+// this will create a form with an input box with the specified
+// placeholder and id. the input's id is whatever id was sent,
+// with 'input' appended to the end of it.
+function createInput(placeholder, id)
+{
+    var form = document.createElement("form");
+    form.id = id;
+    var textBox = document.createElement("input");
+    document.getElementById("terminal").appendChild(form);
+    document.getElementById(form.id).appendChild(textBox);
+    textBox.id = id + "input";
+    textBox.placeholder = placeholder;
+}
 // ------------------------------------------------------------------
 // |                 THE APPLETS THEMSELVES BEGIN HERE              |
 // ------------------------------------------------------------------
@@ -73,4 +89,114 @@ function mathSqrting()
     print("<i style='color:cyan'>let</i> allOverThePlace <span style='color:red'>=</span> somethingStupid;");
     print("<i style='color:cyan'>let</i> Public <span style='color:red'>=</span> <span style='color:lime'>Math</span>.<span style='color:cyan'>sqrt</span>(allOverThePlace);<br>");
     print("Math, sqrting allOverThePlace in Public: " + Public);
+}
+function quizGame()
+{
+    resetTerm();
+    
+    // the header and counter
+    print("<br><i>Java vs Python Quiz Game</i><br>");
+    let counter = 0;
+    
+    // question one
+    print("<i>What is the java equivalent of print()?</i><br>a) System.out.println();<br>b) println();<br>c) std::cout <<;<br>d) printf();")
+    createInput("enter answer here", "q1");
+    q1.addEventListener('submit', function(e) {
+        e.preventDefault();
+        q1.style.display = "none";
+        if (document.getElementById("q1input").value == "a")
+        {
+            print("<span style='color:lime'>correct!</span>");
+            counter++;
+            questionTwo();
+        }
+        else
+        {
+            print("<span style='color:red'>incorrect, or invalid.</span>");
+            questionTwo();
+        }
+    });
+    
+    // question two
+    function questionTwo()
+    {
+        print("<br><i>2. What does Java use instead of functions?</i><br>a) Classes<br>b) Div's<br>c) Java uses functions.<br>d) Methods");
+        createInput("enter answer here", "q2");
+        q2.addEventListener('submit', function(e) {
+            e.preventDefault();
+            q2.style.display = "none";
+            if (document.getElementById("q2input").value == "d")
+            {
+                print("<span style='color:lime'>correct!</span>");
+                counter++;
+                questionThree();
+            }
+            else
+            {
+                print("<span style='color:red'>incorrect, or invalid.</span>");
+                questionThree();
+            }
+        });
+    }
+    
+    function questionThree()
+    {
+        print("<br><i>3. What is the Java if/elif/else equivalent?</i><br>a) if/elif/otherwise<br>b) try/catch<br>c) if/else if/else<br>d) look/elseif/else");
+        createInput("enter answer here", "q3");
+        q3.addEventListener('submit', function(e) {
+            e.preventDefault();
+            q3.style.display = "none";
+            if (document.getElementById("q3input").value == "c")
+            {
+                print("<span style='color:lime'>correct!</span>");
+                counter++;
+                questionFour();
+            }
+            else
+            {
+                print("<span style='color:red'>incorrect, or invalid.</span>");
+                questionFour();
+            }
+        });
+    }
+    function questionFour()
+    {
+        print("<br><i>4. How do you declare a variable in Java?</i><br>a) name = value;<br>b) type name = value<br>c) name type = value<br>d) value = type name");
+        createInput("enter answer here", "q4");
+        q4.addEventListener('submit', function(e) {
+            e.preventDefault();
+            q4.style.display = "none";
+            if (document.getElementById("q4input").value == "b")
+            {
+                print("<span style='color:lime'>correct!</span>");
+                counter++;
+                questionFive();
+            }
+            else
+            {
+                print("<span style='color:red'>incorrect, or invalid.</span>");
+                questionFive();
+            }
+        });
+    }
+    function questionFive()
+    {
+        print("<br><i>5. After importing the correct utility, what command lets you take user input in Java?</i><br>a) Create a variable, and set the value to input.nextInt,Double, or Line.<br>b) variable = System.in();<br>c) std::cin <<;<br>d) userIn()");
+        createInput("enter answer here", "q5");
+        q5.addEventListener('submit', function(e) {
+            e.preventDefault();
+            q5.style.display = "none";
+            if (document.getElementById("q5input").value == "a")
+            {
+                print("<span style='color:lime'>correct!</span>");
+                counter++;
+                print("<i><br>Quiz complete!!!</i><br>You answered " + counter + " out of 5 questions correctly.");
+            }
+            else
+            {
+                print("<span style='color:red'>incorrect, or invalid.</span>");
+                print("<i><br>Quiz complete!!!</i><br>You answered " + counter + " out of 5 questions correctly.");
+            }
+        });
+    }
 }
