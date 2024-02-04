e8990a08374a78ff72492ebf9128332bc404a411
highstake17 <147749937+highstake17@users.noreply.github.com>
2024-02-04 12:22:48 -0700

    ultra super pooper consolidation of ymg code

--- ymg/java.js
+++ ymg/java.js
@@ -1,36 +1,19 @@
 // initialize the list of all buttons
 const buttons = document.getElementsByTagName('button');
 
-// log the buttons to the console
-console.log(buttons);
-
-// constantly scan for button tag clicks, then run newSrc() 
+// loop through every single button and apply the appropriate event listener
 for (let i = 0, len = buttons.length; i < len; i++)
 {
-    if(buttons[i].getAttribute('value') == 'return')
-    {
-        buttons[i].addEventListener('click', outOfIndex);
-    }
-    else if(buttons[i].getAttribute('id') == 'fs')
-    {
-        buttons[i].addEventListener('click', fullScreen);
-    }
-    else if(buttons[i].getAttribute('id') == 'gameReturn')
-    {
-        buttons[i].addEventListener('click', returnToIndex);
-    }
-    else if(buttons[i].getAttribute('class') == 'multigame')
-    {
-        buttons[i].addEventListener('click', openIndex);
-    }
-    else if(buttons[i].getAttribute('id') == 'about')
-    {
-        buttons[i].addEventListener('click', aboutOpen);
-    }
-    else
-    {
-        buttons[i].addEventListener('click', openGame);
-    }
+    if (buttons[i].getAttribute('value') == 'return') buttons[i].addEventListener('click', outOfIndex);
+    else if (buttons[i].getAttribute('id') == 'fs') buttons[i].addEventListener('click', fullScreen);
+    else if (buttons[i].getAttribute('id') == 'gameReturn') buttons[i].addEventListener('click', returnToIndex);
+    else if (buttons[i].getAttribute('class') == 'multigame') buttons[i].addEventListener('click', openIndex);
+    else if (buttons[i].getAttribute('id') == 'about') buttons[i].addEventListener('click', aboutOpen);
+    else if (buttons[i].getAttribute('data-isLink') == "true") buttons[i].addEventListener('click', function(){
+        if (this.getAttribute('data-target') == 'blank') window.open(this.value, '_blank').focus();
+        else location.href = this.value;
+    });
+    else buttons[i].addEventListener('click', openGame);
 }
 
 // assign the button's value to the frm's src, then shut up audio, then clear the menu, and display the window
@@ -46,35 +29,18 @@ function openGame() {
     document.getElementById("fullscreen").style.display ="none";
     document.getElementById("menu").style.display="none";
     let indices = document.getElementsByClassName('multiIndex');
-    for (let i = 0; i < indices.length; i++)
-    {
-        indices[i].style.display = 'none';
-    }
-    if (this.getAttribute('data-isVertical') == 'true')
-    {
-        document.getElementById("frm").style.width = '350px';
-        document.getElementById("frm").style.margin = '0px 0px 0px 375px';
-    }
+    for (let i = 0; i < indices.length; i++) indices[i].style.display = 'none';
+    if (this.getAttribute('data-isVertical') == 'true') document.getElementById("frm").style.width = '350px';
+    else document.getElementById("frm").style.width = '1000px';
 }
 function returnToIndex() {
     document.getElementById("sneaky").muted = false;
     document.getElementById("window").style.display="none";
     let indices = document.getElementsByClassName('multiIndex');
-    for (let i = 0; i < indices.length; i++)
-    {
-        indices[i].style.display = 'none';
-    }
+    for (let i = 0; i < indices.length; i++) indices[i].style.display = 'none';
     document.getElementById('aboutIndex').style.display='none';
-    let multiButtons = document.getElementsByClassName('multigame');
-    
-    if (this.getAttribute('value') == '-1')
-    {
-        document.getElementById("menu").style.display="block";
-    }
-    else if (this.getAttribute('value') == '-5')
-    {
-        document.getElementById('about').style.display='block';
-    }
+    if (this.getAttribute('value') == '-1') document.getElementById("menu").style.display="block";
+    else if (this.getAttribute('value') == '-5') document.getElementById('about').style.display='block';
     else
     {
         let num = Number(this.getAttribute('value'));
@@ -83,8 +49,6 @@ function returnToIndex() {
     }
     document.getElementById("fullframe").src = '';
     document.getElementById("frm").src = '';
-    document.getElementById("frm").style.width = '1000px';
-    document.getElementById("frm").style.margin = '0px 0px 0px 50px';
 }
 function openIndex() {
     let theValue = Number(this.getAttribute('value'));
@@ -109,10 +73,7 @@ function aboutOpen() {
 }
 function outOfIndex() {
     let indices = document.getElementsByClassName('multiIndex');
-    for (let i = 0; i < indices.length; i++)
-    {
-        indices[i].style.display = 'none';
-    }
+    for (let i = 0; i < indices.length; i++) indices[i].style.display = 'none';
     document.getElementById('aboutIndex').style.display='none';
     document.getElementById("window").style.display="none";
     document.getElementById("fullscreen").style.display ="none";
