08745fd07201069b313ba0ad3935bdfe966fb16e
unallegiant <147749937+unallegiant@users.noreply.github.com>
2024-04-11 09:57:59 -0600

    yipee done

--- anger/java.js
+++
@@ -1,9 +0,0 @@
-// JavaScript code for functionality (zzzcode.ai helped with this)
-var form = document.querySelector('form');
-var searchBox = document.querySelector('#box');
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  var searchTerm = searchBox.value;
-  alert(`YEAH ANGER!!!!!!!!!!! BURN IT!!! ARSON!!!!!`);
-  form.reset();
-});
